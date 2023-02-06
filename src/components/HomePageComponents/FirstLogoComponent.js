a2515d226546252b4d2238d21dce1878e61a1583
anakemo <109752478+anakemo@users.noreply.github.com>
2023-02-06 15:04:49 +0400

    creating homepage components/adding markup

---
+++ src/components/HomePageComponents/FirstLogoComponent.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+
+export const FirstLogoComponent = () => {
+  return (
+    <div className='logo1'>
+      
+    </div>
+  )
+}
+
+export const HomePageCompLine = () => {
+    return (
+      <div className='linecomp'>
+        
+        
+      </div>
+    )
+  }
+
+  export const HomepageButtonComp = () => {
+    return (
+      <div className='buttoncomp'>
+        <button type='submit' className='buttoncomptext'> რეზიუმეს დამატება </button>
+        
+      </div>
+    )
+  }
+
+
+ 
+  
+  export const HomePageSecondLogoComponent = () => {
+    return (
+      <div className='secLogo'>
+        
+      </div>
+    )
+  }
+  
+  export default FirstLogoComponent
+  
+
+
---
+++ src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FirstLogoComponent, HomePageCompLine, HomepageButtonComp , HomePageSecondLogoComponent} from '../components/HomePageComponents/FirstLogoComponent'
+
+export const HomePage = () => {
+  return (
+    <div className='homepage'>
+      <FirstLogoComponent/>
+      <HomePageCompLine/>
+      <HomepageButtonComp/>
+      <HomePageSecondLogoComponent/>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
